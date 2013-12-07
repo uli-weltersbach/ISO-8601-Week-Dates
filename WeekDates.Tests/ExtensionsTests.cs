6e0a843187d0f9fc91362bc6d4e6130745d829c7
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-12-07 12:52:18 +0100

    Tests for null handling added.

--- WeekDates.Tests/ExtensionsTests.cs
+++ WeekDates.Tests/ExtensionsTests.cs
@@ -10,17 +10,25 @@ namespace ReasonCodeExample.WeekDates.Tests
         [TestCase("2013-04-02", "2013-04-01", true)]
         [TestCase("2013-04-08", "2013-04-01", false)]
         [TestCase("2013-12-15", "2013-12-16", false)]
+        [TestCase(null, null, false)]
+        [TestCase(null, "2013-12-16", false)]
+        [TestCase("2013-12-16", null, false)]
         public void WeeksAreEquatable(string week, string otherWeek, bool expectedEqualityResult)
         {
             // Arrange
-            WeekDate date = new WeekDateFactory().Create(DateTime.Parse(week));
-            WeekDate otherDate = new WeekDateFactory().Create(DateTime.Parse(otherWeek));
+            WeekDate date = ParseWeekDate(week);
+            WeekDate otherDate = ParseWeekDate(otherWeek);
 
             // Act
-            bool equalityResult = date.IsSameWeekAs(otherDate);
+            bool equalityResult = Extensions.IsSameWeekAs(date, otherDate);
 
             // Assert
             Assert.That(equalityResult, Is.EqualTo(expectedEqualityResult));
+        }
+
+        private WeekDate ParseWeekDate(string week)
+        {
+            return string.IsNullOrEmpty(week) ? null : new WeekDateFactory().Create(DateTime.Parse(week));
         }
     }
 }
